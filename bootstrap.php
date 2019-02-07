9aba78dc7372ee0f0cfb238ec0d97f0c3756f615
Jean luc <you@example.com>
2019-02-07 17:59:44 +0100

    synchro

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require(__DIR__.'/source/class/Construction.php');
+require(__DIR__.'/source/class/Operator.php');
+
+require(__DIR__.'/source/class/Operator/Pour.php');
+
+
+require(__DIR__.'/source/class/Term/x.php');
+
+
+
+
